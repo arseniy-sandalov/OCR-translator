c2fbd7570b5e87b0f60ec9cd063abd7a5c286f11
arseniy-sandalov <arseniy.sandalov@bk.ru>
2025-01-21 17:16:37 +0300

    added HF model path

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 from llm_handler import LLMHandler
 
-MODEL_PATH = "path/to/your/model.gguf"
-
+HF_MODEL_PATH = "unsloth/Llama-3.2-1B-bnb-4bit"
 def main():
-    llm = LLMHandler(model_path=MODEL_PATH)
+    llm = LLMHandler(hf_model_path=HF_MODEL_PATH)
 
     # Call LLM and get response
     response = llm.generate_response(country="France")
