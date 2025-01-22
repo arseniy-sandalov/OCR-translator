167d744915f8606cef34d571e88c3bf1f7a6e055
arseniy-sandalov <arseniy.sandalov@bk.ru>
2025-01-22 10:54:06 +0300

    fixed image path in gradio

--- gradio_interface.py
+++ gradio_interface.py
@@ -1,8 +1,12 @@
 import gradio as gr
 import main
 
+def process_image(image_path):
+    result = main.run_pipeline(image_path)
+    return result
+
 interface = gr.Interface(
-    fn=main.run_pipeline(),
+    fn=process_image,
     inputs=gr.Image(type="filepath"),
     outputs=gr.Image(type="filepath"),
     title="RU-KA Image Translation",
