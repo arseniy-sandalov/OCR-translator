638bcd2c6d0388717b85b72e68f8550ef7417281
arseniy-sandalov <arseniy.sandalov@bk.ru>
2025-01-22 10:49:06 +0300

    adjusted gradio for running in colab

--- gradio_interface.py
+++ gradio_interface.py
@@ -2,11 +2,11 @@ import gradio as gr
 import main
 
 interface = gr.Interface(
-    fn=main.run_pipeline,
+    fn=main.run_pipeline(),
     inputs=gr.Image(type="filepath"),
     outputs=gr.Image(type="filepath"),
     title="RU-KA Image Translation",
     description="Upload an image and see the processed result."
 )
 
-interface.launch()
+interface.launch(share=True)
