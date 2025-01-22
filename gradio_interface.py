94d58b4cbe0722356f26baa985e4a3e3d4202de5
arseniy-sandalov <arseniy.sandalov@bk.ru>
2025-01-22 11:26:44 +0300

    added font path into the interface

--- gradio_interface.py
+++ gradio_interface.py
@@ -1,14 +1,18 @@
 import gradio as gr
 import main
+import os
+
+current_dir = os.getcwd()
+font_path = os.path.join(current_dir, "/content/OCR-translator/NotoSerifGeorgian-VariableFont_wdth,wght.ttf")
 
 def process_image(image_path):
-    result = main.run_pipeline(image_path)
+    result = main.run_pipeline(image_path, font_path)
     return result
 
 interface = gr.Interface(
     fn=process_image,
     inputs=gr.Image(type="filepath"),
-    outputs=gr.Image(type="filepath"),
+    outputs=gr.Image(type="numpy"),
     title="RU-KA Image Translation",
     description="Upload an image and see the processed result."
 )
