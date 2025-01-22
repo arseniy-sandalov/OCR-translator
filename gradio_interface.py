38a453be2af281dbc538c4285fd1766d51882df0
arseniy-sandalov <arseniy.sandalov@bk.ru>
2025-01-22 10:43:52 +0300

    fixed function issue

--- gradio_interface.py
+++ gradio_interface.py
@@ -2,7 +2,7 @@ import gradio as gr
 import main
 
 interface = gr.Interface(
-    fn=main,
+    fn=main.run_pipeline,
     inputs=gr.Image(type="filepath"),
     outputs=gr.Image(type="filepath"),
     title="RU-KA Image Translation",
